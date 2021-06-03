6b5a82d8668a76f677a41870eddc99edb7408e81
Georgy <goshan31458@gmail.com>
2021-06-03 10:41:54 +0300

    Изменения за 02.06.2021

--- spa_bot.py
+++ spa_bot.py
@@ -14,64 +14,54 @@ Google таблица
 import requests
 import pprint
 import telebot
-from telebot import types
 import spa_data
 from telebot.types import Message
-from spa_data import MyUser
 # Переменная с ссылкой на API бота
-BASE_URL = 'https://api.telegram.org/bot1808675918:AAGRq28a2vtiPsUVJLei79D7SK6_xpRhmC8/'
-TOKEN = '1808675918:AAGRq28a2vtiPsUVJLei79D7SK6_xpRhmC8'
-bot = telebot.TeleBot(TOKEN)
 
-
-# словарь пользователей 
-# my_users = {'tg_user_id1': MyUser()}
-
-# Клавиатура главного экрана
-main_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-reg = types.KeyboardButton('Записаться')
-service1 = types.KeyboardButton('Прайс Массажи')
-service2 = types.KeyboardButton('Прайс Уход за внешностью') 
-service3 = types.KeyboardButton('Прайс Бани/Хаммам')
-service4 = types.KeyboardButton('Прайс Эксклюзивные терапии')
-service5 = types.KeyboardButton('Прайс CПА Программы')
-
-main_markup.add(service1, service2, service3, service4, service5, reg)
-
-user = MyUser
+bot = telebot.TeleBot(spa_data.TOKEN)
 
 # Обработка команды /start
 @bot.message_handler(commands=['start'])
-def welcome(message):
-    # my_users[message.chat.id] = MyUser()
-    bot.send_message(message.chat.id, "Здравствуйте, " + str(message.chat.first_name)+"!\nЯ виртуальный администратор SPA салона Алексей!", reply_markup=main_markup)
-    user.state = 'main_menu'
-    print(user.state)
+def welcome(message):  
+    user = spa_data.users_dict.get(message.from_user.id, spa_data.MyUser(message.from_user.id, message.from_user.username))
+    bot.send_photo(user.id, open('img/admin_hi.png', 'rb'))
 
-# Клавиатура экрана записи
-sign_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-back = types.KeyboardButton('👈 Назад')
-call = types.KeyboardButton('☎️ Я хочу позвонить')
-sign_markup.add(back, call)
-
+    bot.send_message(message.chat.id, "Здравствуйте, " + str(message.chat.first_name)+"! Меня зовут Алексей. Я виртуальный администратор SPA комплекса Okura. \nВы можете ознакомиться с прайс листом 📖 и произвести запись на интересующие вас процедуры ", reply_markup = spa_data.main_markup)
+    bot.send_message(message.chat.id, 'В разделе “Записаться” Вы так же можете оставить свой номер телефона и наш оператор вам перезвонит.')
+    user.action(message.chat.id, "main_menu")
 
 # Ответ на запрос прайс-листа
 @bot.message_handler(content_types=['text'])
 def dialog(message):
-    # user = my_users.get(message.chat.id)
-    # user.nextStep()
+    user = spa_data.users_dict.get(message.from_user.id, spa_data.MyUser(message.from_user.id, message.from_user.username))
     l = int()
     try:
         l = list(spa_data.services[message.text].items())
         price = []
         for k in l:
             price.append(k[0]+ ' - ' + k[1] + '\n')
+        bot.send_photo(user.id, open('img/admin_2.png', 'rb'))
         bot.send_message(message.chat.id, 'Готов помочь вам с выбором услуги, ознакомьтесь с нашими ценами! \n \n'+''.join(price))
-    except Exception as er:
-        if message.text.lower() == 'записаться':
-            bot.send_message(message.chat.id, 'Напишите фамилию и имя', reply_markup=sign_markup)
-            user.action(user, message.chat.id, 'sign')
+        user.action(user, message.chat.id, "price_list")
+        print(user.state)
+    except Exception:
+        if message.text == '✍️ Записаться':
+            user.action(message.chat.id, 'sign')
             print(user.state)
     if user.state == 'sign':
-        bot.send_message(message.chat.id, message.text)
+        bot.send_message(message.chat.id, 'Напишите фамилию и имя', reply_markup=spa_data.sign_markup)
+        if message.text.lower() != 'записаться':
+            user.sign_up(message.text)
+            print(str(user.sign_name)) 
+ 
+
+
+# @bot.message_handler(content_types=['text'])
+# def lala(message):
+#     if spa_data.users_dict.get(message.from_user.id) == 'sign':
+#         bot.send_message(message.chat.id, 'Напишите фамилию и имя', reply_markup=spa_data.sign_markup)
+#         user = spa_data.users_dict.get(message.from_user.id)
+#         user.sign_up(message.text)
+
+
 bot.polling()
